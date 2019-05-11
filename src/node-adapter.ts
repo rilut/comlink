4b455b3db2f7437c14d2713c113f7569e7c55c55
Rizky Luthfianto <mrluthfianto@gmail.com>
2019-05-11 23:36:00 +0700

    s/ts/js

--- src/node-adapter.ts
+++ src/node-adapter.ts
@@ -11,7 +11,7 @@
  * limitations under the License.
  */
 
-import { Endpoint } from "./protocol.js";
+import { Endpoint } from "./protocol.ts";
 
 export interface NodeEndpoint {
   postMessage(message: any, transfer?: any[]): void;
